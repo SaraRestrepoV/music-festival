895333ad19a2ee014147732b1dd1f2f40ecd9c5f
Sara <srestrpovelez@gmail.com>
2021-11-07 17:10:14 -0600

    Optimización de imagenes

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const plumber = require('gulp-plumber');
 const cache = require('gulp-cache');
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css( done ) {
     
@@ -25,7 +26,7 @@ function imagenes(done) {
         optimizationLevel: 3
     }
 
-    src('src/img/**/*.{png, jpg}')
+    src('src/img/**/*.{png,jpg}')
         .pipe(cache(imagemin(opciones)))
         .pipe(dest('build/img'))
 
@@ -38,8 +39,21 @@ function versionWebp(done) {
         quality: 50
     };
 
-    src('src/img/**/*.{png, jpg}')
+    src('src/img/**/*.{jpg,png}')
         .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+
+    done();
+}
+
+function versionAvif(done) {
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{jpg,png}')
+        .pipe(avif(opciones))
         .pipe(dest('build/img'))
 
     done();
@@ -53,4 +67,5 @@ function dev(done) {
 exports.css = css;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel(imagenes, versionWebp,dev);
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
